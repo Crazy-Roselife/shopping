7dd0faad3aca7d14509aadf28c87e1a8ad16ec36
admin <admin@itcast.cn>
2020-10-08 17:06:22 +0800

    init shopping

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+/* 路由器对象模块 */
+import Vue from "vue"
+import VueRouter from "vue-router"
+const Msite = () => import("../pages/Msite/Msite.vue")
+const Search = () => import("../pages/Search/Search.vue")
+const Order = () => import("../pages/Order/Order.vue")
+const Profile = () => import("../pages/Profile/Profile.vue")
+const Login = () =>import("../pages/Login/Login.vue")
+//声明使用插件
+Vue.use(VueRouter)
+//2.创建router
+const routes = [
+  {
+    path:"",
+    redirect:"/msite"
+    
+  },
+  {
+    path:"/msite",
+    component: Msite,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path:"/search",
+    component: Search,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path:"/order",
+    component: Order,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path:"/profile",
+    component: Profile,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path:"/login",
+    component: Login
+  }
+]
+const router = new VueRouter({
+  routes,
+  mode: "history"
+})
+export default router
